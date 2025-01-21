ec32e3f279a36eefa639d9d5eac64102feb55e13
Temleitner Marcell <temmar20@premontrei-keszthely.local>
2025-01-21 10:18:01 +0100

    Fixed userroutes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -4,21 +4,9 @@ import { authenticateJwt } from '../middlewares/authMiddleware';
 
 export const userRoutes = async (fastify: FastifyInstance) => {
   fastify.post('/register', registerUser);
-
   fastify.post('/login', loginUser);
-
   fastify.delete('/delete', { preHandler: authenticateJwt }, deleteUser);
-
-  fastify.put('/new', { preHandler: authenticateJwt }, updateUser);
-
-  fastify.get('/profile', { preHandler: authenticateJwt }, profile);
-
-  fastify.get('/user/:username', getUserByUsername);
-  
-  fastify.post('/login', loginUser);
-
-  fastify.delete('/delete', { preHandler: authenticateJwt }, deleteUser);
-  fastify.put('/new', { preHandler: authenticateJwt }, updateUser);
   fastify.get('/profile', { preHandler: authenticateJwt }, profile);
   fastify.get('/user/:username', getUserByUsername);
+  fastify.put('/new', { preHandler: authenticateJwt }, updateUser);
 };
