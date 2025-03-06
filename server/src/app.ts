68dcad711fe0840eb1cd6506cbf56fcc3eeb22af
Temleitner Marcell <premoprojekt2024@gmail.com>
2025-03-06 15:37:36 +0100

    Updated registerUser with ProfilePic generation

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,15 @@
-
-import cors from '@fastify/cors';
-import websocket from '@fastify/websocket';
-import * as dotenv from 'dotenv';
-import Fastify from 'fastify';
-import 'reflect-metadata';
-import { chatController } from './controllers/chatController';
-import { userRoutes } from './routes/userRoutes';
-import { chatRoutes } from './routes/chatRoutes';
-import { friendRoutes } from './routes/friendRoutes';
-import { storeRoutes } from './routes/storeRoutes';
-import { availRoutes } from './routes/availRoutes';
-import { appointmentRoutes } from './routes/appointmentRoutes';
-
+import cors from "@fastify/cors";
+import websocket from "@fastify/websocket";
+import * as dotenv from "dotenv";
+import Fastify from "fastify";
+import "reflect-metadata";
+import { chatController } from "./controllers/chatController";
+import { userRoutes } from "./routes/userRoutes";
+import { chatRoutes } from "./routes/chatRoutes";
+import { friendRoutes } from "./routes/friendRoutes";
+import { storeRoutes } from "./routes/storeRoutes";
+import { availRoutes } from "./routes/availRoutes";
+import { appointmentRoutes } from "./routes/appointmentRoutes";
 
 dotenv.config();
 
@@ -20,29 +18,23 @@ const fastify = Fastify({
 });
 
 fastify.register(websocket, {
-  options: { maxPayload: 1048576 }  
+  options: { maxPayload: 1048576 },
 });
-
 
 fastify.register(cors, {
   origin: "http://localhost:5173",
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,  
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 });
 
-
-
-
-
 fastify.register(chatController);
-fastify.register(userRoutes, { prefix: '/api/v1' });
-fastify.register(chatRoutes, { prefix: '/api/v1' });
-fastify.register(storeRoutes, { prefix: '/api/v1' });
-fastify.register(friendRoutes, { prefix: '/api/v1' });
-fastify.register(availRoutes, { prefix: '/api/v1' } );
-fastify.register(appointmentRoutes, { prefix: '/api/v1' } );
-
+fastify.register(userRoutes, { prefix: "/api/v1" });
+fastify.register(chatRoutes, { prefix: "/api/v1" });
+fastify.register(storeRoutes, { prefix: "/api/v1" });
+fastify.register(friendRoutes, { prefix: "/api/v1" });
+fastify.register(availRoutes, { prefix: "/api/v1" });
+fastify.register(appointmentRoutes, { prefix: "/api/v1" });
 
 fastify.listen({ port: 8080 }, (err, address) => {
   if (err) {
--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,29 +1,51 @@
-import bcrypt from 'bcryptjs';
-import { FastifyReply, FastifyRequest } from 'fastify';
-import { AppDataSource } from '../config/dbconfig';
-import { generateToken } from '../middlewares/authMiddleware';
-import * as model from '../models/index'; 
-import { loginSchema, registerSchema, updateSchema } from '../shared/validation/userValidation';
-import { AuthenticatedRequest} from '../interfaces/interfaces';
+import bcrypt from "bcryptjs";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { AppDataSource } from "../config/dbconfig";
+import { generateToken } from "../middlewares/authMiddleware";
+import * as model from "../models/index";
+import {
+  loginSchema,
+  registerSchema,
+  updateSchema,
+} from "../shared/validation/userValidation";
+import { AuthenticatedRequest } from "../interfaces/interfaces";
 
-
-
-export const registerUser = async (request: FastifyRequest, reply: FastifyReply) => {
+export const registerUser = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
   const result = registerSchema.safeParse(request.body);
   if (!result.success) {
-    return reply.status(400).send({ message: 'Validation failed', errors: result.error.errors });
+    return reply
+      .status(400)
+      .send({ message: "Validation failed", errors: result.error.errors });
   }
 
   const { username, email, password, role } = result.data;
 
-  if (role !== 'client' && role !== 'worker') {
-    return reply.status(400).send({ message: 'Invalid role. Must be either "client" or "worker".' });
+  if (role !== "client" && role !== "worker") {
+    return reply
+      .status(400)
+      .send({ message: 'Invalid role. Must be either "client" or "worker".' });
   }
 
-  const existingUser = await AppDataSource.getRepository(model.User).findOneBy({ email });
-  if (existingUser) {
-    return reply.status(400).send({ message: 'User already exists' });
+  const existingEmail = await AppDataSource.getRepository(model.User).findOneBy(
+    {
+      email,
+    },
+  );
+  if (existingEmail) {
+    return reply.status(400).send({ message: "Email already exists" });
   }
+
+  const existingUser = await AppDataSource.getRepository(model.User).findOneBy({
+    username,
+  });
+  if (existingUser) {
+    return reply.status(400).send({ message: "Username already exists" });
+  }
+
+  const generateProfilepic = `https://ui-avatars.com/api/?name=${username[0]}&size=128`;
 
   try {
     const salt = await bcrypt.genSalt(12);
@@ -32,67 +54,79 @@ export const registerUser = async (request: FastifyRequest, reply: FastifyReply)
     newUser.username = username;
     newUser.email = email;
     newUser.password = hashedPassword;
-    newUser.role = role;  
-
+    newUser.role = role;
+    newUser.profilePic = generateProfilepic;
     await AppDataSource.getRepository(model.User).save(newUser);
 
-   
-    if (role === 'worker') {
+    if (role === "worker") {
       const newExtendedWorker = new model.ExtendedWorker();
       newExtendedWorker.user = newUser;
-      newExtendedWorker.description = '';
-      await AppDataSource.getRepository(model.ExtendedWorker).save(newExtendedWorker);
-    
-    } else {
-      return reply.status(400).send({ message: 'Invalid role. Must be either "client" or "worker".' });
+      newExtendedWorker.description = "";
+      await AppDataSource.getRepository(model.ExtendedWorker).save(
+        newExtendedWorker,
+      );
     }
 
-    return reply.status(201).send({ message: 'User created successfully' });
+    return reply.status(201).send({ message: "User created successfully" });
   } catch (error) {
-    console.error('Error during user registration:', error);
-    return reply.status(500).send({ message: 'Error creating user' });
+    console.error("Error during user registration:", error);
+    return reply.status(500).send({ message: "Error creating user" });
   }
 };
 
-export const loginUser = async (request: FastifyRequest, reply: FastifyReply) => {
+export const loginUser = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
   const result = loginSchema.safeParse(request.body);
   if (!result.success) {
-    return reply.status(400).send({ message: 'Validation failed', errors: result.error.errors });
+    return reply
+      .status(400)
+      .send({ message: "Validation failed", errors: result.error.errors });
   }
 
   const { email, password } = result.data;
 
   try {
-    const user = await AppDataSource.getRepository(model.User).findOneBy({ email });
+    const user = await AppDataSource.getRepository(model.User).findOneBy({
+      email,
+    });
 
     if (!user) {
-      return reply.status(400).send({ message: 'Invalid credentials' });
+      return reply.status(400).send({ message: "Invalid credentials" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return reply.status(400).send({ message: 'Invalid credentials' });
+      return reply.status(400).send({ message: "Invalid credentials" });
     }
 
     const token = generateToken(user);
 
-    return reply.send({ message: 'Login successful', token });
+    return reply.send({ message: "Login successful", token });
   } catch (error) {
-    console.error('Error during login:', error);
-    return reply.status(500).send({ message: 'An error occurred during login' });
+    console.error("Error during login:", error);
+    return reply
+      .status(500)
+      .send({ message: "An error occurred during login" });
   }
 };
 
-export const updateUser = async (request: AuthenticatedRequest, reply: FastifyReply) => {
+export const updateUser = async (
+  request: AuthenticatedRequest,
+  reply: FastifyReply,
+) => {
   const userId = request.user?.userId;
 
   if (!userId) {
-    return reply.status(400).send({ message: 'User ID is missing' });
+    return reply.status(400).send({ message: "User ID is missing" });
   }
 
   const result = updateSchema.safeParse(request.body);
   if (!result.success) {
-    return reply.status(400).send({ message: 'Validation failed', errors: result.error.errors });
+    return reply
+      .status(400)
+      .send({ message: "Validation failed", errors: result.error.errors });
   }
 
   const { username, email, password } = result.data;
@@ -102,7 +136,7 @@ export const updateUser = async (request: AuthenticatedRequest, reply: FastifyRe
     const user = await userRepository.findOneBy({ userId: userId });
 
     if (!user) {
-      return reply.status(404).send({ message: 'User not found' });
+      return reply.status(404).send({ message: "User not found" });
     }
 
     if (username) user.username = username;
@@ -114,33 +148,38 @@ export const updateUser = async (request: AuthenticatedRequest, reply: FastifyRe
     }
 
     await userRepository.save(user);
-    return reply.send({ message: 'User updated successfully', user });
+    return reply.send({ message: "User updated successfully", user });
   } catch (error) {
-    console.error('Error during user update:', error);
-    return reply.status(500).send({ message: 'An error occurred while updating user' });
+    console.error("Error during user update:", error);
+    return reply
+      .status(500)
+      .send({ message: "An error occurred while updating user" });
   }
 };
 
-export const deleteUser = async (request: AuthenticatedRequest, reply: FastifyReply) => {
+export const deleteUser = async (
+  request: AuthenticatedRequest,
+  reply: FastifyReply,
+) => {
   const userId = request.user?.userId;
 
   if (!userId) {
-    return reply.status(400).send({ message: 'User ID is missing' });
+    return reply.status(400).send({ message: "User ID is missing" });
   }
   try {
     const userRepository = AppDataSource.getRepository(model.User);
     const user = await userRepository.findOneBy({ userId: userId });
 
     if (!user) {
-      return reply.status(404).send({ message: 'User not found' });
+      return reply.status(404).send({ message: "User not found" });
     }
 
     await userRepository.remove(user);
-    return reply.send({ message: 'User deleted successfully' });
+    return reply.send({ message: "User deleted successfully" });
   } catch (error) {
-    console.error('Error during user deletion:', error);
-    return reply.status(500).send({ message: 'An error occurred while deleting user' });
+    console.error("Error during user deletion:", error);
+    return reply
+      .status(500)
+      .send({ message: "An error occurred while deleting user" });
   }
 };
-
-
