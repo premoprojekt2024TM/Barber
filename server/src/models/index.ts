cc1d9ea3d025447a57d5e89d7052f806251bb2c0
Besze Marcell <besmar20@premontrei-keszthely.local>
2025-04-01 08:46:02 +0200

    módosított index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,8 +1,6 @@
 export { User } from "./User";
 export { Store } from "./Store";
 export { Friendship } from "./Friendship";
-export { ChatRoom } from "./ChatRoom";
-export { Message } from "./message";
 export { StoreWorker } from "./StoreWorker";
 export { AvailabilityTimes } from "./Availability";
 export { Appointment } from "./Appointment";
