259c9b901edb18a423fbb40240532518ed22bcd2
Besze Marcell <besmar20@premontrei-keszthely.local>
2025-04-01 09:52:07 +0200

    véges userroutes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -4,31 +4,18 @@ import {
   loginUser,
   registerUser,
   updateUser,
-  isConnectedToStore,
   getCurrentUser,
-  isStoreOwner,
 } from "../controllers/authController";
 import { authenticateJwt, authorizeRole } from "../middlewares/authMiddleware";
 
 export const userRoutes = async (fastify: FastifyInstance) => {
   fastify.post("/register", registerUser);
   fastify.post("/login", loginUser);
-  fastify.get(
-    "/isStoreOwner",
-    { preHandler: [authenticateJwt, authorizeRole(["worker"])] },
-    isStoreOwner,
-  );
-  fastify.get(
-    "/is-connected-to-store",
-    { preHandler: authenticateJwt },
-    isConnectedToStore,
-  );
   fastify.delete("/delete", { preHandler: authenticateJwt }, deleteUser);
   //fastify.get("/profile", { preHandler: authenticateJwt }, profile);
   //fastify.get('/user/:username', getUserByUsername);
   fastify.put("/update", { preHandler: authenticateJwt }, updateUser);
   fastify.get("/me", { preHandler: authenticateJwt }, getCurrentUser);
-  //test routes
   fastify.get(
     "/client",
     { preHandler: [authenticateJwt, authorizeRole(["client"])] },
