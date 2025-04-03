3dc554b42313acbc898b785dabb292b184e80d08
Besze Marcell <besmar20@premontrei-keszthely.local>
2025-04-03 09:21:33 +0200

    deleted routes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -12,8 +12,6 @@ export const userRoutes = async (fastify: FastifyInstance) => {
   fastify.post("/register", registerUser);
   fastify.post("/login", loginUser);
   fastify.delete("/delete", { preHandler: authenticateJwt }, deleteUser);
-  //fastify.get("/profile", { preHandler: authenticateJwt }, profile);
-  //fastify.get('/user/:username', getUserByUsername);
   fastify.put("/update", { preHandler: authenticateJwt }, updateUser);
   fastify.get("/me", { preHandler: authenticateJwt }, getCurrentUser);
   fastify.get(
